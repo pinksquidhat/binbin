9eaaa9c62f8371da6e377cb56713589a4fec4bc5
Scott Baldwin <pinksquidhat@gmail.com>
2017-07-05 20:25:06 -0400

    Refactored a bit

--- test/binbin.js
+++ test/binbin.js
@@ -4,12 +4,12 @@ const { bb, decode, encode } = require('../lib/binbin');
 describe('decode', () => {
   it('decodes on boundaries', () => {
     const sampleData = [parseInt('00001111', 2), parseInt('00100001', 2)];
-    const decoded = decode([
+    const decoded = decode(bb.sequence(
       ['a', bb.bit4],
       ['b', bb.bit4],
       ['c', bb.bit4],
       ['d', bb.bit4]
-    ], sampleData);
+    ), sampleData);
 
     assert.equal(decoded.a, 0);
     assert.equal(decoded.b, 15);
@@ -18,14 +18,14 @@ describe('decode', () => {
   })
   it('decodes simple bits', () => {
     const sampleData = [parseInt('00001111', 2), parseInt('01101100', 2)];
-    const decoded = decode([
+    const decoded = decode(bb.sequence(
       ['a', bb.bit4],
-      ['b', [
+      ['b', bb.sequence(
         ['bb', bb.bit1]
-      ]],
+      )],
       ['c', bb.bit4],
       ['d', bb.bit7]
-    ], sampleData);
+    ), sampleData);
 
     assert.equal(decoded.a, 0);
     assert.equal(decoded.b.bb, 1);
@@ -34,19 +34,19 @@ describe('decode', () => {
   });
   it('decodes uint8', () => {
     const sampleData = [13, 37];
-    const decoded = decode([
+    const decoded = decode(bb.sequence(
       ['a', bb.uint8],
       ['b', bb.uint8]
-    ], sampleData);
+    ), sampleData);
 
     assert.equal(decoded.a, 13);
     assert.equal(decoded.b, 37);
   });
   it('decodes array of bits', () => {
     const sampleData = [parseInt('10101011', 2), parseInt('01011101', 2)];
-    const decoded = decode([
+    const decoded = decode(bb.sequence(
       ['a', bb.array(16, bb.bit1)]
-    ], sampleData);
+    ), sampleData);
     
     assert.deepEqual(decoded.a, [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1])
   })
